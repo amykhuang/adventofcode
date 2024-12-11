94943ba9a1b767e95ca877d25f31976c39ef38a8
Amy Huang <amykhuang@gmail.com>
2024-12-10 20:49:44 -0800

    add day01

---
+++ 2024/day01.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### Day 1: Historian Hysteria ###
+
+INPUTFILE=$1
+
+paste -d'-' \
+  <(cat $INPUTFILE | tr -s ' ' | cut -d' ' -f1 | sort -n) \
+  <(cat $INPUTFILE | tr -s ' ' | cut -d' ' -f2 | sort -n) \
+  | bc \
+  | tr -d '-' \
+  | awk '{ sum += $1 } END { print sum }'
+
+while read ct n; do
+  declare "count_$n=$ct"
+  var="count_$n"
+done < <(cat $INPUTFILE | tr -s ' ' | cut -d' ' -f2 | sort | uniq -c)
+
+paste -sd'+' <(
+while read n; do
+  var="count_$n"
+  if [ ! -z ${!var} ]; then
+    echo "${!var}*$n"
+  fi
+done < <(cat $INPUTFILE | tr -s ' ' | cut -d' ' -f1)
+) | bc
